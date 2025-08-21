e66a57a61c46f07f8c1c8a146a75b2ed9838b378
Ramon <ramon.fernando98@gmail.com>
2025-08-21 07:07:38 -0300

    Hangman game done

--- Desafio 02/hangman/src/main/java/br/com/ramon/hangman/Main.java
+++ Desafio 02/hangman/src/main/java/br/com/ramon/hangman/Main.java
@@ -1,11 +1,16 @@
 package br.com.ramon.hangman;
 
+import br.com.ramon.hangman.exception.GameIsFinishedException;
+import br.com.ramon.hangman.exception.LetterAlreadyInputtedException;
 import br.com.ramon.hangman.model.HangmanChar;
 import br.com.ramon.hangman.model.HangmanGame;
 
+import java.util.Scanner;
 import java.util.stream.Stream;
 
 public class Main {
+
+    private final static Scanner scanner = new Scanner(System.in);
 
     public static void main(String... args) {
         var characters = Stream.of(args)
@@ -13,14 +18,40 @@ public class Main {
                 .map(HangmanChar::new).toList();
         System.out.println(characters);
         var hangmanGame = new HangmanGame(characters);
+        System.out.println("Bem vindo ao jogo da forca, tente adivinhar a palavra, boa sorte");
         System.out.println(hangmanGame);
-        hangmanGame.inputCharacter('t');
-        System.out.println(hangmanGame);
-        hangmanGame.inputCharacter('e');
-        System.out.println(hangmanGame);
-        hangmanGame.inputCharacter('s');
-        System.out.println(hangmanGame);
-        hangmanGame.inputCharacter('t');
+        var option = -1;
+        while (true) {
+            System.out.println("Selecione uma das opções:");
+            System.out.println("1 - Informar uma letra");
+            System.out.println("2 - Verificar status do jogo");
+            System.out.println("3 - Sair do jogo");
+            option = scanner.nextInt();
+            switch (option) {
+                case 1 -> inputCharacter(hangmanGame);
+                case 2 -> showGameStatus(hangmanGame);
+                case 3 -> System.exit(0);
+                default ->  System.out.println("Opção inválida");
+            }
+        }
+    }
 
+    private static void showGameStatus(HangmanGame hangmanGame) {
+        System.out.println(hangmanGame.getHangmanGameStatus());
+        System.out.println(hangmanGame);
+    }
+
+    private static void inputCharacter(HangmanGame hangmanGame) {
+        System.out.println("Informe uma letra:");
+        var character = scanner.next().charAt(0);
+        try {
+            hangmanGame.inputCharacter(character);
+        } catch (LetterAlreadyInputtedException ex) {
+            System.out.println(ex.getMessage());
+        } catch (GameIsFinishedException ex) {
+            System.out.println(ex.getMessage());
+            System.exit(0);
+        }
+        System.out.println(hangmanGame);
     }
 }

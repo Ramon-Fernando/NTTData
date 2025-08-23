430c91853108b2189e2b94d7b5a5c320d1d2dff5
Ramon <ramon.fernando98@gmail.com>
2025-08-23 04:23:43 -0300

    Product entity done

---
+++ Desafio 03/product-service/src/main/java/com/nttdata/challenge/product_service/entities/Product.java
@@ -0,0 +1,57 @@
+package com.nttdata.challenge.product_service.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    private Double price;
+
+    public Product() {
+    }
+
+    public Product(Long id, String name, String description, Double price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Product)) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}

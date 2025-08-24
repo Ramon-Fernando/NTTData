3f97ecbdea919eb6759329068ff15483bf5df062
Ramon <ramon.fernando98@gmail.com>
2025-08-24 07:41:30 -0300

    Order entity done

---
+++ Desafio 03/order-service/src/main/java/com/nttdata/challenge/order_service/entities/Order.java
@@ -0,0 +1,62 @@
+package com.nttdata.challenge.order_service.entities;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_order")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant moment;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private Set<OrderItem> items = new HashSet<>();
+
+    public Order() {
+    }
+
+    public Order(Long id, Instant moment) {
+        this.id = id;
+        this.moment = moment;
+        this.items = items;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public List<OrderItem> getItems() {
+        return items;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Order)) return false;
+        Order order = (Order) o;
+        return Objects.equals(id, order.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}

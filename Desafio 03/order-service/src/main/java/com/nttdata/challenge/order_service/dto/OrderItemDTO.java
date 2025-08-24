212301ffb1d5d0033a7ba1e0d5d1bd1eb7466512
Ramon <ramon.fernando98@gmail.com>
2025-08-24 17:30:57 -0300

    Some fixes in DTO

--- Desafio 03/order-service/src/main/java/com/nttdata/challenge/order_service/dto/OrderItemDTO.java
+++ Desafio 03/order-service/src/main/java/com/nttdata/challenge/order_service/dto/OrderItemDTO.java
@@ -16,7 +16,15 @@ public class OrderItemDTO {
         return productId;
     }
 
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
     public Integer getQuantity() {
         return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
     }
 }
--- Desafio 03/order-service/src/main/java/com/nttdata/challenge/order_service/dto/ProductDTO.java
+++ Desafio 03/order-service/src/main/java/com/nttdata/challenge/order_service/dto/ProductDTO.java
@@ -21,15 +21,31 @@ public class ProductDTO {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getNome() {
         return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
     public String getDescricao() {
         return descricao;
     }
 
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
     public Double getPreco() {
         return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
     }
 }

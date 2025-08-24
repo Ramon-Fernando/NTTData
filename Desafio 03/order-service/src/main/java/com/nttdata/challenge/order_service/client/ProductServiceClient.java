ac74d37dd5230a51d7cadf6d42e41a33c413d13f
Ramon <ramon.fernando98@gmail.com>
2025-08-24 17:39:37 -0300

    ProductServiceClient done

---
+++ Desafio 03/order-service/src/main/java/com/nttdata/challenge/order_service/client/ProductServiceClient.java
@@ -0,0 +1,18 @@
+package com.nttdata.challenge.order_service.client;
+
+import com.nttdata.challenge.order_service.dto.ProductDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name = "product-service", path = "/produtos")
+public interface ProductServiceClient {
+
+    @GetMapping
+    List<ProductDTO> findAll();
+
+    @GetMapping("/{id}")
+    ProductDTO findById(@PathVariable("id") Long id);
+}
